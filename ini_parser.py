706dde9ee2057e1bbc9a1687e5a7f0d7a1f2af28
Omar <omarabdul3ziz@gmail.com>
2019-07-10 12:21:03 +0200

    Implement ini parse

---
+++ ini_parser.py
@@ -0,0 +1,40 @@
+sample = """
+[author]
+name = xmonader
+email = notxmonader@gmail.com
+[general]
+appname = configparser
+version = 0.1
+"""
+sample = sample.splitlines()
+
+class Parser:
+    def __init__(self, sample):
+        self.sample = sample
+    
+    def parse(self):
+        lines = []
+        for line in sample :
+            line = line.strip()
+            if line == "":
+                continue
+            else :
+                lines.append(line)
+
+        main_dict = {}
+        section = ""
+        for line in  lines:
+            if line.startswith("["):
+                section = ""
+                section = line[1:-1]
+                main_dict[section] = {}
+            else:
+                key , value = line.split("=")
+                main_dict[section][key.strip()] = value.strip()
+        return main_dict
+
+
+text = Parser(sample)
+
+from pprint import pprint
+pprint(text.parse())
